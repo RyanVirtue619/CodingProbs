6410ddf3e99d327f1fed7869323dffd2c3897e80
RyanVirtue619 <113137954+RyanVirtue619@users.noreply.github.com>
2023-10-13 18:00:05 -0700

    2 probs

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+
+int32_t main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); 
+    cout.tie(0);
+    //Start coding
+    int n;
+    string s;
+    cin >> n;
+    for (int i = 0; i < n; i ++) {
+        cin >> s;
+        int size = len(s);
+        if (size > 10) {
+            cout << s[0] << size-2 << s[size-1] << endl;
+        } else {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
