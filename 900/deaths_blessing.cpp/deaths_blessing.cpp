ee85a8925410e4d15dd0c84292d274ba6604cfda
RyanVirtue619 <113137954+RyanVirtue619@users.noreply.github.com>
2023-10-25 18:57:56 -0700

    db

--- 900/deaths_blessing.cpp/deaths_blessing.cpp
+++ 900/deaths_blessing.cpp/deaths_blessing.cpp
@@ -23,7 +23,7 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     //Start coding
-    //https://codeforces.com/problemset/problem/1849/B
+    //https://codeforces.com/problemset/problem/1749/B
     ll t;
     cin >> t;
     while(t--) {
