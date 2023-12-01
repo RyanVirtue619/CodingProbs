67a3cd53782707409050f5ae166aa64fd9c19419
RyanVirtue619 <113137954+RyanVirtue619@users.noreply.github.com>
2023-12-01 13:30:17 -0800

    n

--- 1700/sum_of_xor_funcs/sumxor.cpp
+++ 1700/sum_of_xor_funcs/sumxor.cpp
@@ -20,15 +20,18 @@ int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     //Start coding
     ll n;
     cin >> n;
-    vll vec(n+1);
-    vec[0] = 0;
-    for(int i = 1; i < n+1; i++) {
+    vll vec(n);
+    for(int i = 0; i < n+1; i++) {
         ll b;
         cin >> b;
-        vec[i] = vec[i-1] ^ b;
+        vec[i] = b;
     }
     ll ans = 0;
     for(int k = 0; k < 30; k++) {
@@ -49,7 +52,7 @@ int32_t main()
             }
             sumfirst += countfirst;
             sumsecond += countsecond;
-            sumbit = sumbit + sumfirst; 
+            sumbit = (sumbit + sumfirst) % mod; 
         }
         // scale answer to current bit
         ans = (ans + (sumbit * (1 << k))) % mod;
